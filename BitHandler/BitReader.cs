ebc8f7f78de358f2acdcb976befb918aeb57f7e2
dana.diac <dana.diac@bearingpoint.com>
2016-03-22 23:40:46 +0200

    reader

---
+++ BitHandler/BitReader.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitHandler
+{
+    public class BitReader
+    {
+        private BitArray readBuffer;
+        private int noOfBitsRead;
+        private string filePath;
+        private int fileBookmark;
+        private FileStream file;
+        public BitReader(string path)
+        {
+            fileBookmark = 0;
+            noOfBitsRead = 0;
+            filePath = path;
+            file = File.Open(filePath, FileMode.Open);
+
+        }
+
+        public bool? Read()
+        {
+            var auxArray = new byte[1];
+            if (noOfBitsRead % 8 == 0)
+            {
+                if (file.CanRead && fileBookmark != file.Length)
+                {
+                    
+                    auxArray[0] = (byte)file.ReadByte();
+                    readBuffer = new BitArray(auxArray);
+                    noOfBitsRead++;
+                    fileBookmark++;
+                    return readBuffer[0];
+                }
+                else
+                    return null;
+
+            }
+            noOfBitsRead++;
+            return readBuffer[(noOfBitsRead - 1) % 8];
+        }
+
+    }
+}
---
+++ TestBitHandler/Program.cs
@@ -0,0 +1,27 @@
+﻿using BitHandler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestBitHandler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = new BitReader("Test.txt");
+            bool? bla;
+            do
+            {
+               bla = x.Read();
+                if(bla!=null)
+               Console.Write((bool)bla ? 1 : 0);
+                
+            }while (bla != null) ;
+
+
+        }
+    }
+}
